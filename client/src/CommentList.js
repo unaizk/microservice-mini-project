e4c564a52e60799c466c7d779b450e23a47853af
root <root@Unaislaptop>
2023-09-13 17:40:56 +0530

    now listing posts and its depended comments was querying from one service query service so it is not depended to post service and comments service

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,20 +1,8 @@
 import React,{useState , useEffect} from 'react'
-import axios from 'axios'
 
-const CommentList = ({postId}) => {
-    const [comment,setComment] = useState([])
-
-    const fetchData = async()=>{
-        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
-
-        setComment(res.data)
-    }
-
-    useEffect(()=>{
-        fetchData()
-    },[])
-
-    const renderedComments = comment.map((comment)=>{
+const CommentList = ({comments}) => {
+   
+    const renderedComments = comments.map((comment)=>{
         return(
             <li key={comment.id}>{comment.content}</li>
         )
