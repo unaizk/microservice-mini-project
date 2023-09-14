3919cfb4de33ab877f40ad32b532b5dc007df9c0
root <root@Unaislaptop>
2023-09-14 15:45:28 +0530

    just created moderator sevice to check or flag the specific comments we mentioned

--- comments/index.js
+++ comments/index.js
@@ -27,7 +27,7 @@ app.post('/posts/:id/comments',async(req,res)=>{
 
    const comments = commentsByPostId[req.params.id] || []
 
-   comments.push({id:commentId, content});
+   comments.push({id:commentId, content, status:'Pending'});
 
    commentsByPostId[req.params.id] = comments ;
 
@@ -36,15 +36,41 @@ app.post('/posts/:id/comments',async(req,res)=>{
         data:{
             id : commentId,
             content,
-            postId : req.params.id
+            postId : req.params.id,
+            status : 'Pending'
         }
    })
 
    res.status(201).send(comments);
 })
 
-app.post('/events',(req,res)=>{
+app.post('/events',async(req,res)=>{
     console.log('Recieved Events', req.body.type);
+
+    const {type , data} = req.body;
+
+    if(type === 'CommentModerated'){
+
+        const {id , content , postId , status} = data;
+
+        const comments = commentsByPostId[postId];
+        const comment = comments.find((commentId)=>{
+            return id === commentId.id
+        })
+        comment.status = status
+
+        await axios.post('http://localhost:4005/events',{
+            type : "CommentUpdated",
+            data : {
+                id,
+                status,
+                postId,
+                content
+            }
+        }).catch((err)=>{
+            console.log(err.message);
+        })
+    }
 
     res.send({})
 })
