c7546f70250c22dd21318c21122e3381501867ba
root <root@Unaislaptop>
2023-09-16 21:07:54 +0530

    created yaml file for creating kubernates cluster and pods and services to communicate each container inside cluster and created node port communicate from outside cluster

--- event-bus/index.js
+++ event-bus/index.js
@@ -13,16 +13,16 @@ app.post("/events", (req, res) => {
 
   events.push(event)
 
-  axios.post("http://localhost:4000/events", event).catch((err) => {
+  axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4001/events", event).catch((err) => {
+  axios.post("http://comments-srv:4001/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4002/events", event).catch((err) => {
+  axios.post("http://query-srv:4002/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4003/events", event).catch((err) => {
+  axios.post("http://moderation-srv:4003/events", event).catch((err) => {
     console.log(err.message);
   });
   res.send({ status: "OK" });
@@ -33,5 +33,6 @@ app.get('/events',(req,res)=>{
 })
 
 app.listen(4005, () => {
+  console.log('v5');
   console.log("Listening on 4005");
 });
